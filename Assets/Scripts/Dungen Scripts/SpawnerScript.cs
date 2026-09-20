504c41f209ce556dcf14cd3b1a565d682c29ee67
Psylones <map60944@gmail.com>
2026-09-21 11:04:52 +1200

    spawner

--- Assets/Scripts/Dungen Scripts/SpawnerScript.cs
+++ Assets/Scripts/Dungen Scripts/SpawnerScript.cs
@@ -1,18 +1,42 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SpawnerScript : MonoBehaviour
 {
 
-    QuestManager questManager;
+    //QuestManager questManager;
+
+    [SerializeField] private GameObject swarmerPrefab;
+    [SerializeField] private GameObject bigSwarmerPrefab;
+    GameManager gameManager;
+    private float MaxEnemies = 6;
+    private int EnemiesSpawned;
+    private int EnemiesSpawning;
+
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void Start()
     {
-        questManager = FindFirstObjectByType<QuestManager>();   
+        Debug.Log("spawner working");
+        gameManager = FindFirstObjectByType<GameManager>();   
+        MaxEnemies -= (gameManager.TownMorale /= 20);
+
+        Debug.Log("Max Enemies = " + MaxEnemies);
+        float randomFloat = Random.Range(0, MaxEnemies);
+        int roundValue = Mathf.RoundToInt(randomFloat);
+        EnemiesSpawning = (int)randomFloat;
+        Debug.Log("Enemies Spawning = " + EnemiesSpawning);
+        SpawnEnemies();
+
     }
 
-    // Update is called once per frame
-    void Update()
+   public void SpawnEnemies()
     {
-        
+        while (EnemiesSpawning < EnemiesSpawned)
+        {
+            Instantiate(swarmerPrefab);
+            EnemiesSpawned++;
+        }
     }
 }
